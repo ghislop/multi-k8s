4eb26ba8c5a9f196bc6d50674e9669c06ca572d9
George Hislop <ghislop@catalystsolutions.com>
2020-08-27 14:17:26 -0600

    travis and deploy sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t ghislop4/multi-client:latest -t ghislop4/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ghislop4/multi-server:latest -t ghislop4/multi-server:$SHA  -f ./Server/Dockerfile ./Server
+docker build -t ghislop4/multi-worker:latest -t ghislop4/multi-worker:$SHA  -f ./worker/Dockerfile ./worker
+docker push ghislop4/multi-client:latest
+docker push ghislop4/multi-client:$SHA
+docker push ghislop4/multi-server:latest
+docker push ghislop4/multi-server:$SHA
+docker push ghislop4/multi-worker:latest
+docker push ghislop4/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=ghislop4/multi-server:$SHA
+kubectl set image deployments/client-deployment client=ghislop4/multi-slient:$SHA
+kubectl set image deployments/worker-deployment worker=ghislop4/multi-worker:$SHA
